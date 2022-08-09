b3a2aed11a2c9f1266fb886fc081836f45ac8981
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-08-09 12:25:07 +0600

    connect mongodb create collection and create app.get show ui on childtype

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,6 +9,39 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASSWORD}@cluster0.1pttxds.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// client.connect((err) => {
+//   const collection = client.db("child-adoption-system").collection("all_child");
+//   // perform actions on the collection object
+//   console.log("env file mongodb connected");
+//   client.close();
+// });
+
+async function run() {
+  try {
+    await client.connect();
+    const childCollection = client
+      .db("child-adoption-system")
+      .collection("all_child");
+
+    app.get("/child/:childType", async (req, res) => {
+      const childType = req.params.childType
+      
+      const query = {childType:childType};
+      const result = await childCollection.find(query).toArray();
+      res.send(result);
+    });
+  } finally {
+    //
+  }
+}
+run().catch(console.dir);
 
 // get response server root path
 app.get("/", (req, res) => {
